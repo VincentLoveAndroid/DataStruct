bbc5aff1f097e922b2b780ac0521dfb203944a09
VincentLoveAndroid <674928145@qq.com>
2017-03-12 10:39:33 +0800

    2

--- app/src/main/java/vincent/datastruct/Sort/QuickSort.java
+++ app/src/main/java/vincent/datastruct/Sort/QuickSort.java
@@ -5,7 +5,9 @@ import java.util.Arrays;
 /**
  * Created by vincent on 2017/3/10.
  * email-address:674928145@qq.com
- * description:是对冒泡排序的一种改进，同属于交换排序
+ * description:是对冒泡排序的一种改进，同属于交换排序，每次以第一或者最后一个元素作为基准元素
+ * 前后两个游标移动和基准元素比较，每一趟排序直到所有比基准元素小的元素在其前面，所有
+ * 比基准元素大的元素在其后面，然后确定基准元素位置后，基准元素前后两个区间再分别进行排序，如此递归。
  */
 
 public class QuickSort {
@@ -18,32 +20,38 @@ public class QuickSort {
         count++;
         if (leftPoint < rightPoint) {
             int middle = getMiddle(arr, leftPoint, rightPoint);
-            quickSort(arr, leftPoint,middle - 1);
+            //每次排序根据确定好的基准元素的位置再分前后两个区间，分别进行排序，递归进行。
+            quickSort(arr, leftPoint, middle - 1);
             quickSort(arr, middle + 1, rightPoint);
         }
-
-
     }
 
+    /**
+     * @param arr
+     * @param leftPoint  左游标，同时也是基准元素的初始位置
+     * @param rightPoint 右游标
+     * @return 一趟排序，确定基准元素的位置，并返回该位置
+     */
     static int getMiddle(int[] arr, int leftPoint, int rightPoint) {
         int temp = arr[leftPoint];
         while (leftPoint < rightPoint) {
-            while (leftPoint < rightPoint && temp < rightPoint) {
+            //右游标数组大于基准元素，往前移，直到小于基准元素，发生交换
+            while (leftPoint < rightPoint && temp < arr[rightPoint]) {
                 rightPoint--;
             }
             arr[leftPoint] = arr[rightPoint];
             arr[rightPoint] = temp;
-
-            while (leftPoint < rightPoint && temp > leftPoint) {
+           //左游标数组小于基准元素，往后移，直到大于基准元素，发生交换
+            while (leftPoint < rightPoint && temp > arr[leftPoint]) {
                 leftPoint++;
             }
             arr[rightPoint] = arr[leftPoint];
             arr[leftPoint] = temp;
         }
 
-        int middle = leftPoint;
-        arr[leftPoint] = temp;
-        return middle;
+        int middle = leftPoint;//此时左右游标重合
+        arr[leftPoint] = temp;//确定基准元素的位置
+        return middle;//返回一趟排序后基准元素的位置
     }
 
     public static void main(String arg[]) {
