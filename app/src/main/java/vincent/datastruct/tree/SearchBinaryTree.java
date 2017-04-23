95923e00c0dadf25a7ebc4bd1c09b649a5a9c7f3
VincentLoveAndroid <674928145@qq.com>
2017-04-23 23:35:23 +0800

    图

--- app/src/main/java/vincent/datastruct/tree/SearchBinaryTree.java
+++ app/src/main/java/vincent/datastruct/tree/SearchBinaryTree.java
@@ -13,32 +13,73 @@ import java.util.List;
 
 public class SearchBinaryTree {
 
-    static List<HuffmanTree.Node> nodeList = new ArrayList<>();
-    private static Node bindNote;
+    static List<Node> nodeList = new ArrayList<>();
 
     static class Node {
-        HuffmanTree.Node leftChild;
-        HuffmanTree.Node rightChild;
-        int weighted;//权值
-        String data;//数据大小
+        Node leftChild;
+        Node rightChild;
+        int data;//数据大小
 
-        public Node(String data, int weighted) {
+        public Node(int data) {
             this.data = data;
-            this.weighted = weighted;
         }
     }
 
     private static void initNodeList() {
-        nodeList.add(new HuffmanTree.Node("A", 20));
-        nodeList.add(new HuffmanTree.Node("B", 5));
-        nodeList.add(new HuffmanTree.Node("C", 4));
-        nodeList.add(new HuffmanTree.Node("D", 6));
-        nodeList.add(new HuffmanTree.Node("E", 3));
+        nodeList.add(new Node(20));
+        nodeList.add(new Node(5));
+        nodeList.add(new Node(4));
+        nodeList.add(new Node(6));
+        nodeList.add(new Node(3));
     }
 
     public static void main(String arg[]) {
         initNodeList();
+        Node rootNode = createSearchBinaryTree();
+        preOrder(rootNode);
+    }
 
+    /**
+     * 创建查找二叉树
+     *
+     * @return
+     */
+    private static Node createSearchBinaryTree() {
+        Node rootNode = null;
+        for (Node node : nodeList) {
+            if (rootNode == null) {
+                //如果没有根节点，初始化根节点并跳出此次遍历
+                rootNode = node;
+                continue;
+            }
+            Node parent = null;
+            //每次从根节点开始查找
+            Node currentNode = rootNode;
+            //根据小左大右的规则，循环找到叶子节点（左孩子或右孩子为空），即为待插入节点的双亲节点
+            while (currentNode != null) {
+                parent = currentNode;
+                if (node.data < currentNode.data) {
+                    currentNode = currentNode.leftChild;
+                } else {
+                    currentNode = currentNode.rightChild;
+                }
+            }
+            //这时parent即为待插入节点的双亲节点
+            if (node.data < parent.data) {
+                parent.leftChild = node;
+            } else {
+                parent.rightChild = node;
+            }
+        }
+        return rootNode;
+    }
+
+
+    private static void preOrder(Node node) {
+        if (node == null) return;
+        System.out.println("preOrder:" + "data:" + node.data);
+        preOrder(node.leftChild);
+        preOrder(node.rightChild);
     }
 
 }
--- app/src/main/java/vincent/datastruct/tree/Test.java
+++ app/src/main/java/vincent/datastruct/tree/Test.java
@@ -1,7 +1,5 @@
 package vincent.datastruct.tree;
 
-import vincent.datastruct.map.HashMap;
-import vincent.datastruct.map.HashMap2;
 
 /**
  * Created by vincent on 2017/2/23.
@@ -12,14 +10,5 @@ import vincent.datastruct.map.HashMap2;
 public class Test {
 
     public static void main(String[] arg) {
-        HashMap2<Integer, Integer> map = new HashMap2<>();
-        for (int i = 0; i < 1000; i++) {
-            map.put(i, i);
-        }
-
-        for (int i = 0; i < 1000; i++) {
-            System.out.println(map.get(i));
-        }
-
     }
 }
